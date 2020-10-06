bc064a2cd63f3bc4b4106c8e41ca0a540cabf4c0
joaolummertz1@gmail.com <joaolummertz1@gmail.com>
2020-10-06 00:46:46 -0300

    Fix: improved promotion

--- src/application/Application.java
+++ src/application/Application.java
@@ -15,10 +15,10 @@ public class Application {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		
+
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-		
+
 		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
@@ -26,40 +26,42 @@ public class Application {
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(scanner);
-				
+
 				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
-				
+
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(scanner);
-				
+
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-				
+
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-				
+
 				if (chessMatch.getPromoted() != null) {
 					System.out.println("Enter piece for promotion (B/N/R/Q): ");
-					String type = scanner.nextLine();
+					String type = scanner.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.println("Invalid value. Enter piece for promotion (B/N/R/Q): ");
+						type = scanner.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
-			}
-			catch (ChessException e) {
-			System.out.println(e.getMessage());
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
 				scanner.nextLine();
-			}
-			catch (InputMismatchException e) {
+			} catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				scanner.nextLine();
 			}
 		}
-		
+
 		UI.clearScreen();
 		UI.printMatch(chessMatch, captured);
-		
+
 	}
-	
+
 }
